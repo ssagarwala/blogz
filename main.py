07eb5eee383c9ac66d6586233c4d3530b6d2866f
Sehar <sfsial@gmail.com>
2018-04-17 12:35:17 -0500

    added styles

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import cgi
+from datetime import datetime
+from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
@@ -13,14 +15,18 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1200))
+    pub_date = db.Column(db.DateTime)
    
-    def __init__(self, title,body):
+    def __init__(self, title,body,pub_date=None):
         self.title = title
         self.body = body 
+        if pub_date is None:
+           pub_date = datetime.utcnow()
+        self.pub_date = pub_date
 
 
 def get_current_bloglist():
-    return Blog.query.all()
+    return Blog.query.order_by(desc(Blog.pub_date)).all()
 
 @app.route('/add-blog', methods=['POST'])
 def add_blog():
